9ab8222d30829798de84074297a2b99d447b4f9d
Suraj <suraj.suraj.4.4.1996@gmail.com>
2022-01-19 03:17:45 +0530

    Day6::Daily_Practise_Problems

---
+++ MySQL/Day6.sql
@@ -0,0 +1,15 @@
+-- Abbreviation
+-- dept_emp(emp_no, dept_no, from_date, to_date) as depe, 
+-- dept_manager(dept_no, emp_no, from_date, to_date) as depm
+--, employees(emp_no, birth_date, first_name, last_name, gender) as emp, 
+-- salaries(emp_no, salary, from_date, to_date) as sal, 
+-- titles(emp_no, title, from_date, to_date) as tis.
+
+--1. We'd like to offer ESOPs to all the top performing employees
+-- Criterion: Income >= 71046 and have worked atleast 10 years at the firm as a Senior Engineer.
+-- Approach : 1st join the tables : salaries and titles then sort by salary and difference between the dates.
+
+select sal.emp_no, tis.title, sal.salary, sal.from_date, sal.to_date
+FROM salaries as sal
+INNER JOIN titles tis ON sal.emp_no = tis.emp_no
+WHERE tis.title='Senior Engineer' and date(sal.to_date) - date(sal.from_date)> 365*10;
